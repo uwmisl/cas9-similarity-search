799f0508e541322b755b2800649c049b6bbff1e4
J.D <jdunstan@cs.washington.edu>
2021-05-26 14:28:02 -0700

    Added dependency on tables.

--- primo/models/simulator.py
+++ primo/models/simulator.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-import cupyck
-from cupyck.session.session import Session
+# import cupyck
+# from cupyck.session.session import Session
 
 from ..tools import sequences as seqtools
 
@@ -84,53 +84,53 @@ class Simulator:
 
         return conc_jobs.join(duplex_yields)
 
-if __name__ == "__main__":
-    # This allows you to use this simulator as either a client or a server.
-    import argparse
+# if __name__ == "__main__":
+#     # This allows you to use this simulator as either a client or a server.
+#     import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("max_seqlen", type=int)
-    parser.add_argument("port", type=int)
-    parser.add_argument("--nblocks", type=int)
-    parser.add_argument("--nthreads", type=int)
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("max_seqlen", type=int)
+#     parser.add_argument("port", type=int)
+#     parser.add_argument("--nblocks", type=int)
+#     parser.add_argument("--nthreads", type=int)
 
-    parser.add_argument("--rp", type=str)
-    parser.add_argument("--toehold", type=str)
-    parser.add_argument("--t_conc", type=float)
-    parser.add_argument("--q_conc", type=float)
-    parser.add_argument("--temp", type=float)
+#     parser.add_argument("--rp", type=str)
+#     parser.add_argument("--toehold", type=str)
+#     parser.add_argument("--t_conc", type=float)
+#     parser.add_argument("--q_conc", type=float)
+#     parser.add_argument("--temp", type=float)
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    sess_args = {
-        "max_seqlen": args.max_seqlen,
-        "nblocks": args.nblocks
-    }
-    if args.nthreads is not None:
-        sess_args['nthreads'] = args.nthreads
+#     sess_args = {
+#         "max_seqlen": args.max_seqlen,
+#         "nblocks": args.nblocks
+#     }
+#     if args.nthreads is not None:
+#         sess_args['nthreads'] = args.nthreads
 
-    sim_args = {}
-    if args.rp is not None:
-        sim_args['rp'] = args.rp
-    if args.toehold is not None:
-        sim_args['toehold'] = args.toehold
-    if args.t_conc is not None:
-        sim_args['t_conc'] = args.t_conc
-    if args.q_conc is not None:
-        sim_args['q_conc'] = args.q_conc
-    if args.temp is not None:
-        sim_args['temp'] = args.temp
+#     sim_args = {}
+#     if args.rp is not None:
+#         sim_args['rp'] = args.rp
+#     if args.toehold is not None:
+#         sim_args['toehold'] = args.toehold
+#     if args.t_conc is not None:
+#         sim_args['t_conc'] = args.t_conc
+#     if args.q_conc is not None:
+#         sim_args['q_conc'] = args.q_conc
+#     if args.temp is not None:
+#         sim_args['temp'] = args.temp
 
-    try:
-        session = cupyck.GPUSession(**sess_args)
-    except RuntimeError:
-        print("GPU startup failed. falling back to multicore backend.")
-        session = cupyck.MulticoreSession()
+#     try:
+#         session = cupyck.GPUSession(**sess_args)
+#     except RuntimeError:
+#         print("GPU startup failed. falling back to multicore backend.")
+#         session = cupyck.MulticoreSession()
 
-    simulator = Simulator(session, **sim_args)
-    class SimServer(cupyck.Server):
-        def worker(self, jobs):
-            return simulator.simulate(jobs)
+#     simulator = Simulator(session, **sim_args)
+#     class SimServer(cupyck.Server):
+#         def worker(self, jobs):
+#             return simulator.simulate(jobs)
 
-    server = SimServer(args.port, session)
-    server.listen(verbose=True)
+#     server = SimServer(args.port, session)
+#     server.listen(verbose=True)
