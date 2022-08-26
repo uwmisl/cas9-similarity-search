935b41de1bf52d73ce098132023835beb8c276e5
Lee Organick <leeorg@cs.washington.edu>
2022-08-26 11:10:37 -0700

    fixed typos

--- notebooks/04_experiments/css_analysis_functions.py
+++ notebooks/04_experiments/css_analysis_functions.py
@@ -8,26 +8,26 @@ from tqdm import tqdm # for progress bar
 import matplotlib.pyplot as plt
 
 from Bio.SeqIO import parse
-from Bio.Seq import Seq 
+from Bio.Seq import Seq
 from Bio import pairwise2
 
 
-SCORE_THRESH = 7 # my rule of thumb, this value is 
-                # len(query) - 3 
-                # for queries >= 20nt but less for shorter queries. 
-                # Note that the score threshold will only return sequences 
-                # with alignment score > threshold so if your query is 20nt 
+SCORE_THRESH = 7 # my rule of thumb, this value is
+                # len(query) - 3
+                # for queries >= 20nt but less for shorter queries.
+                # Note that the score threshold will only return sequences
+                # with alignment score > threshold so if your query is 20nt
                 # your maximum alignment score will be 20
 
-MAX_READ_LEN = 500 # often the sequence aligner chokes on long sequences, this is the maximum 
+MAX_READ_LEN = 500 # often the sequence aligner chokes on long sequences, this is the maximum
                     # length a sequence is allowed to be in order to be analyzed, longer reads
                     # are totally ignored
 
 
 def gz_extract(nanopore_data_path):
     """
-    For a given directory, unzip all .gz files in folder, 
-    save unzipped files in folder and delete zipped files. 
+    For a given directory, unzip all .gz files in folder,
+    save unzipped files in folder and delete zipped files.
     """
     directory = nanopore_data_path + '/fastq_pass'
     extension = ".gz"
